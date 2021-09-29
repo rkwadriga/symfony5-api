1796ba2234408067d2f18be54719d5e90106c9d4
rkwadriga <dmitry.kushneriov@obreey-products.com>
2021-09-29 16:16:49 +0300

    Add "Cheese" entity and fix db link

---
+++ src/Entity/CheeseListing.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CheeseListingRepository;
+use Doctrine\ORM\Mapping as ORM;
+use \DateTimeInterface;
+
+#[ApiResource(
+    collectionOperations: [
+        "GET" => ["path" => "/I❤cheeses/{id}"],
+        "POST"
+    ],
+    itemOperations: ["GET", "PUT"],
+    shortName: "cheeses"
+)]
+/**
+ * @ORM\Entity (repositoryClass=CheeseListingRepository::class)
+ */
+class CheeseListing
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $description;
+
+    /**
+     * The price of this delicious cheese, in cents
+     *
+     * @ORM\Column(type="integer")
+     */
+    private int $price;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTimeInterface $createdAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $isPublished;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getIsPublished(): bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
