1d8a7f5b86fab7d93cf862a5aa51da0226e25133
rkwadriga <dmitry.kushneriov@obreey-products.com>
2021-10-19 00:13:50 +0300

    Decorating Data Persisters vs Context Builders

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -8,13 +8,12 @@ namespace App\DataPersister;
 
 use ApiPlatform\Core\DataPersister\DataPersisterInterface;
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class UserDataPersister implements DataPersisterInterface
 {
     public function __construct(
-        private EntityManagerInterface $entityManager,
+        private DataPersisterInterface $decoratedDataPersister,
         private PasswordHasherFactoryInterface $encoder
     ) {}
 
@@ -34,13 +33,11 @@ class UserDataPersister implements DataPersisterInterface
             $data->eraseCredentials();
         }
 
-        $this->entityManager->persist($data);
-        $this->entityManager->flush();
+        $this->decoratedDataPersister->persist($data);
     }
 
     public function remove($data)
     {
-        $this->entityManager->remove($data);
-        $this->entityManager->flush();
+        $this->decoratedDataPersister->remove($data);
     }
 }
