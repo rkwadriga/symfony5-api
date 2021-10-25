90d31859f5f2287eb9f3d4aff7f27cf306567d53
rkwadriga <dmitry.kushneriov@obreey-products.com>
2021-10-25 19:23:52 +0300

    Custom Filter, getDescription() & properties

--- src/ApiPlatform/CheeseSearchFilter.php
+++ src/ApiPlatform/CheeseSearchFilter.php
@@ -6,19 +6,34 @@
 
 namespace App\ApiPlatform;
 
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
 
-class CheeseSearchFilter extends AbstractFilter
+class CheeseSearchFilter extends AbstractContextAwareFilter
 {
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
-        // TODO: Implement filterProperty() method.
+        if ($property !== 'search') {
+            return;
+        }
+
+        $alias = $queryBuilder->getRootAliases()[0];
+        // A param name that is generated unique in this query
+        $valueParameter = $queryNameGenerator->generateParameterName('serach');
+        $queryBuilder
+            ->andWhere("{$alias}.title LIKE :{$valueParameter} OR {$alias}.description LIKE :{$valueParameter}")
+            ->setParameter($valueParameter, "%{$value}%")
+        ;
     }
 
     public function getDescription(string $resourceClass): array
     {
-
+        return ['search' => [
+            'property' => null,
+            'type' => 'string',
+            'required' => false,
+            'description' => 'Search across multiple fields',
+        ]];
     }
 }
